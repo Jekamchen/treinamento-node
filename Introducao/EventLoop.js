be24baa43294e8a868ae527c717987da725ae4f9
Jonathan Bach <jo.bach.s@gmail.com>
2022-02-22 10:55:53 -0300

    Alterações no event loop

--- Introducao/EventLoop.js
+++ Introducao/EventLoop.js
@@ -10,13 +10,19 @@ function sleep(milliseconds) {
 function primeiroBloco(){
 	// Aguardar 5 segundos
     setTimeout( function(){
-        sleep(5000)
         console.log('Esse é o primeiro bloco de comandos após 5 segundos');
-    }, 1000 );
+    }, 5000 );
     
 }
 function segundoBloco(){
 	console.log('Esse é o segundo bloco de comandos');
 }
+
+function terceiroBloco(){
+	setTimeout( function(){
+        console.log('Esse é o terceiro bloco')
+    }, 2000);
+}
 primeiroBloco();
 segundoBloco();
+terceiroBloco();
